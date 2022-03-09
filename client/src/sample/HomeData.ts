43ef4f1c19a3aa88ce33200bacb9d16132e452bd
Christopher Obando <chrisjobando@gmail.com>
2022-03-09 07:48:44 -0800

    temp: add mock data

---
+++ client/src/sample/HomeData.ts
@@ -0,0 +1,12 @@
+import { WorkoutSimple } from "../types";
+
+export const user: string = "Chris";
+export const savedWorkouts: WorkoutSimple[] = [
+  { id: "1", title: "Monday", numExercises: 7 },
+  { id: "2", title: "Tuesday", numExercises: 6 },
+  { id: "3", title: "Wednesday", numExercises: 8 },
+  { id: "4", title: "Thursday", numExercises: 6 },
+  { id: "5", title: "Friday", numExercises: 7 },
+  { id: "6", title: "Saturday", numExercises: 5 },
+];
+export const pastWorkouts: WorkoutSimple[] = [];
