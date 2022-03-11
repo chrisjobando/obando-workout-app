ff5958cc98ed851cc8bfcc1934920366e88f94bb
Christopher Obando <chrisjobando@gmail.com>
2022-03-11 09:13:03 -0800

    feat: add uuid and update example data

--- client/src/sample/HomeData.ts
+++ client/src/sample/HomeData.ts
@@ -1,12 +1,279 @@
-import { WorkoutSimple } from "../types";
+import { WorkoutSimple, WorkoutFull } from "../types";
+import { v4 as uuidv4 } from "uuid";
+
+const ids = {
+  1: uuidv4(),
+  2: uuidv4(),
+  3: uuidv4(),
+  4: uuidv4(),
+  5: uuidv4(),
+  6: uuidv4(),
+};
 
 export const user: string = "Chris";
+
 export const savedWorkouts: WorkoutSimple[] = [
-  { id: "1", title: "Monday", numExercises: 7 },
-  { id: "2", title: "Tuesday", numExercises: 6 },
-  { id: "3", title: "Wednesday", numExercises: 8 },
-  { id: "4", title: "Thursday", numExercises: 6 },
-  { id: "5", title: "Friday", numExercises: 7 },
-  { id: "6", title: "Saturday", numExercises: 5 },
+  { id: ids[1], name: "Monday", numExercises: 7 },
+  { id: ids[2], name: "Tuesday", numExercises: 6 },
+  { id: ids[3], name: "Wednesday", numExercises: 8 },
+  { id: ids[4], name: "Thursday", numExercises: 6 },
+  { id: ids[5], name: "Friday", numExercises: 7 },
+  { id: ids[6], name: "Saturday", numExercises: 5 },
 ];
+
+export const savedWorkoutsFull: WorkoutFull[] = [
+  {
+    id: ids[1],
+    name: "Monday",
+    numExercises: 7,
+    exercises: [
+      {
+        name: "Deadlift",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+      {
+        name: "Push-up",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+      {
+        name: "Pull-up",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+      {
+        name: "Squat",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+      {
+        name: "Curl",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+      {
+        name: "Leg Press",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+      {
+        name: "Crunch",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+    ],
+  },
+  {
+    id: ids[2],
+    name: "Tuesday",
+    numExercises: 6,
+    exercises: [
+      {
+        name: "Deadlift",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+      {
+        name: "Push-up",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+      {
+        name: "Pull-up",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+      {
+        name: "Squat",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+      {
+        name: "Curl",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+      {
+        name: "Leg Press",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+    ],
+  },
+  {
+    id: ids[3],
+    name: "Wednesday",
+    numExercises: 5,
+    exercises: [
+      {
+        name: "Deadlift",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+      {
+        name: "Push-up",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+      {
+        name: "Pull-up",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+      {
+        name: "Squat",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+    ],
+  },
+  {
+    id: ids[4],
+    name: "Thursday",
+    numExercises: 6,
+    exercises: [
+      {
+        name: "Deadlift",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+      {
+        name: "Push-up",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+      {
+        name: "Pull-up",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+      {
+        name: "Squat",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+      {
+        name: "Curl",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+      {
+        name: "Leg Press",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+    ],
+  },
+  {
+    id: ids[5],
+    name: "Friday",
+    numExercises: 7,
+    exercises: [
+      {
+        name: "Deadlift",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+      {
+        name: "Push-up",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+      {
+        name: "Pull-up",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+      {
+        name: "Squat",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+      {
+        name: "Curl",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+      {
+        name: "Leg Press",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+      {
+        name: "Crunch",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+    ],
+  },
+  {
+    id: ids[6],
+    name: "Saturday",
+    numExercises: 5,
+    exercises: [
+      {
+        name: "Deadlift",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+      {
+        name: "Push-up",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+      {
+        name: "Pull-up",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+      {
+        name: "Squat",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+      {
+        name: "Curl",
+        repetitions: [10, 10, 10, 10],
+        rests: [30, 30, 30],
+        sets: 4,
+      },
+    ],
+  },
+];
+
 export const pastWorkouts: WorkoutSimple[] = [];
